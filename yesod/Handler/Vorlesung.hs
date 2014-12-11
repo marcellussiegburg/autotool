3cf4cb731706044b42d4ff66aae2d939b050e78c
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2014-12-11 12:27:14 +0100

    Änderung der Navigation

--- yesod/Handler/Vorlesung.hs
+++ yesod/Handler/Vorlesung.hs
@@ -5,7 +5,6 @@ import Import
 
 data VorlesungForm = VorlesungForm {
     name :: Text,
-    semester :: Int,
     beginn :: Day,
     beginnZeit :: TimeOfDay,
     ende :: Day,
@@ -19,7 +18,7 @@ getVorlesungR vorlesung = do
       beginnZeit' = midnight
       ende' = fromGregorian 2014 12 31
       endeZeit' = midnight
-      vorlesung' = Just $ VorlesungForm "Theoretische Grundlagen der Informatik" 1 beginn' beginnZeit' ende' endeZeit' $ Just "Keine"
+      vorlesung' = Just $ VorlesungForm "Theoretische Grundlagen der Informatik" beginn' beginnZeit' ende' endeZeit' $ Just "Keine"
   (formWidget, formEnctype) <- generateFormPost $ vorlesungForm vorlesung'
   defaultLayout $ do
     $(widgetFile "vorlesung")
@@ -30,17 +29,15 @@ postVorlesungR vorlesung = do
       beginnZeit' = midnight
       ende' = fromGregorian 2014 12 31
       endeZeit' = midnight
-      vorlesung' = Just $ VorlesungForm "Theoretische Grundlagen der Informatik" 1 beginn' beginnZeit' ende' endeZeit' $ Just "Keine"
+      vorlesung' = Just $ VorlesungForm "Theoretische Grundlagen der Informatik" beginn' beginnZeit' ende' endeZeit' $ Just "Keine"
   ((result, formWidget), formEnctype) <- runFormPost $ vorlesungForm vorlesung'
   defaultLayout $ do
     $(widgetFile "vorlesung")
 
 vorlesungForm :: Maybe VorlesungForm -> Form VorlesungForm
 vorlesungForm mvorlesung = do
-  let vorlesungen = [("WS 2014/2015", 1), ("SS 2014", 2), ("WS 2013/2014", 3), ("SS 2013", 4), ("WS 2012/2013", 5)] :: [(Text, Int)]
   renderBootstrap3 BootstrapBasicForm $ VorlesungForm
     <$> areq textField (bfs MsgVorlesungName) (fmap name mvorlesung)
-    <*> areq (selectFieldList vorlesungen) (bfs MsgSemester) (fmap semester mvorlesung)
     <*> areq (jqueryDayField def) (bfsFormControl MsgEinschreibungBeginnDatum) (fmap beginn mvorlesung)
     <*> areq timeField (bfs MsgEinschreibungBeginnZeit) (fmap beginnZeit mvorlesung)
     <*> areq (jqueryDayField def) (bfsFormControl MsgEinschreibungEndeDatum) (fmap ende mvorlesung)
