1f8aa6ca873a8e793a67329263e92434be2fa48d
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2015-01-20 17:33:38 +0100

    Implementation der Auswahl der Schule (vorerst ohne Auswahl vor Login)

--- yesod/Handler/Schulen.hs
+++ yesod/Handler/Schulen.hs
@@ -1,8 +1,31 @@
 module Handler.Schulen where
 
 import Import
+import Control.Schule.DB as SchuleDB
+import Control.Schule.Typ as Schule
+import Control.Types
 
 getSchulenR :: Handler Html
 getSchulenR = do
+  schulen <- lift $ SchuleDB.get
+  mid <- maybeAuthId
+  schulenAutorisiert' <- lift $ mapM (autorisiertSchule mid) schulen
+  let schulenAutorisiert = concat schulenAutorisiert'
   defaultLayout $ do
     $(widgetFile "schulen")
+
+autorisiertSchule :: Maybe (AuthId Autotool) -> Schule -> IO [(Text, Maybe (Route Autotool), Maybe (Route Autotool))]
+autorisiertSchule mid schule = do
+  let UNr u = Schule.unr schule
+      Name sname = Schule.name schule
+      semesterRoute = SemestersR u
+      bearbeitenRoute = SchuleR u
+      ist (Just True) route = Just route
+      ist _ _ = Nothing
+  autorisiertS <- istAutorisiert mid semesterRoute
+  autorisiertB <- istAutorisiert mid bearbeitenRoute
+  if autorisiertS == Just True || autorisiertB == Just True
+     then return [(pack sname
+                  ,ist autorisiertS semesterRoute
+                  ,ist autorisiertB bearbeitenRoute)] 
+     else return []
--- yesod/Import.hs
+++ yesod/Import.hs
@@ -4,6 +4,7 @@ module Import
 
 import Prelude as Import hiding (head, init, last, readFile, sequence, tail, writeFile, foldl, foldr, mapM)
 import Yesod as Import
+import Yesod.Auth as Import (AuthId, maybeAuthId)
 import Yesod.Form.Bootstrap3 as Import (BootstrapFormLayout (..), BootstrapGridOptions (..), BootstrapSubmit (BootstrapSubmit), bfs, bootstrapSubmit, renderBootstrap3, withAutofocus)
 import Yesod.Form.Jquery as Import (jqueryDayField, def)
 import Text.Blaze.Html as Import (ToMarkup, preEscapedToHtml)
@@ -11,7 +12,7 @@ import Text.Shakespeare.Text as Import (toText)
 
 import Control.Applicative as Import (pure, (<$>), (<*>), (<*), (*>))
 import Data.Foldable as Import (foldl, foldr)
-import Data.Maybe as Import (listToMaybe, maybeToList)
+import Data.Maybe as Import (isJust, listToMaybe, maybeToList)
 import Data.Text as Import (Text, pack, unpack)
 import Data.Text.Lazy.Builder as Import (toLazyText)
 import Data.Time as Import (Day, TimeOfDay, UTCTime (UTCTime), fromGregorian, midnight, timeOfDayToTime)
