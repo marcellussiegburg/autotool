a181af351c51e00600166e9c46c9b1fa64fba1ba
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2015-01-25 23:41:53 +0100

    Basisimplementation der Gruppenauswahl kopiert

--- yesod/Handler/Gruppen.hs
+++ yesod/Handler/Gruppen.hs
@@ -1,12 +1,27 @@
 module Handler.Gruppen where
 
 import Import
-import Prelude (undefined)
+import qualified Control.Gruppe.DB as GruppeDB
+import qualified Control.Gruppe.Typ as Gruppe
+import qualified Control.Vorlesung.DB as VorlesungDB
+import qualified Control.Vorlesung.Typ as Vorlesung
+import Control.Types
 
 getGruppenR :: VorlesungId -> Handler Html
-getGruppenR vorlesung = do
-  defaultLayout $ do
-    $(widgetFile "gruppen")
+getGruppenR = postGruppenR
 
 postGruppenR :: VorlesungId -> Handler Html
-postGruppenR vorlesung = undefined
+postGruppenR vorlesung = do
+  mid <- maybeAuthId
+  gruppen <- lift $ GruppeDB.get_this $ VNr vorlesung
+  gruppenBesucht <- lift $ fmap (concat . maybeToList) $ mapM (\ms -> GruppeDB.get_attended (VNr vorlesung) (SNr ms)) mid
+  istTutor' <- lift $ istAutorisiert mid $ VorlesungR vorlesung
+  vorlesung' <- lift $ VorlesungDB.get_this $ VNr vorlesung
+  let istTutor = istTutor' == Just True
+      fromName name = let Name n = name
+                      in n
+      fromGNr gnr = let GNr nr = gnr
+                    in nr
+      gruppenBesuchtIds = map Gruppe.gnr gruppenBesucht
+  defaultLayout $ do
+    $(widgetFile "gruppen")
