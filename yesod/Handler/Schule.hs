b31bd5348a60e1948156de3ef5632ba711772c23
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2015-02-01 23:28:10 +0100

    Refaktorisierung des Anlegen und Bearbeiten von Schulen

--- yesod/Handler/Schule.hs
+++ yesod/Handler/Schule.hs
@@ -1,30 +1,44 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Handler.Schule where
 
 import Import
+import qualified Control.Schule.DB as SchuleDB
+import Control.Schule.Typ as Schule
+import Control.Types
+import Autolib.Multilingual
 
-data SchuleForm = SchuleForm {
-    name :: Text,
-    suffix :: Maybe Text
-}
+spracheMsg :: Language -> AutotoolMessage
+spracheMsg sprache' = case sprache' of
+  DE -> MsgSpracheDE
+  NL -> MsgSpracheNL
+  UK -> MsgSpracheUK
+
+spracheOptionen :: Handler (OptionList Language)
+spracheOptionen = optionsPairs $ map (\s -> (spracheMsg s, s)) [DE, NL, UK]
 
 getSchuleR :: SchuleId -> Handler Html
-getSchuleR schule = do
-  let schule' = Just $ SchuleForm "HTWK Leipzig" $ Just "htwk-leipzig.de"
-  (formWidget, formEnctype) <- generateFormPost $ schuleForm schule'
-  defaultLayout $ do
-    $(widgetFile "schule")
+getSchuleR = postSchuleR
 
 postSchuleR :: SchuleId -> Handler Html
 postSchuleR schule = do
-  let schule' = Just $ SchuleForm "HTWK Leipzig" $ Just "htwk-leipzig.de"
-  ((result, formWidget), formEnctype) <- runFormPost $ schuleForm schule'
+  mschule <- lift $ liftM listToMaybe $ SchuleDB.get_unr $ UNr schule
+  ((result, formWidget), formEnctype) <- runFormPost $ schuleForm mschule
+  case result of
+    FormMissing -> return ()
+    FormFailure _ -> return ()
+    FormSuccess schule' -> do
+      _ <- lift $ SchuleDB.put (Just $ unr schule') schule'
+      setMessageI MsgSchuleBearbeitet
   defaultLayout $ do
     $(widgetFile "schule")
 
-schuleForm :: Maybe SchuleForm -> Form SchuleForm
+schuleForm :: Maybe Schule -> Form Schule
 schuleForm mschule = do
-  renderBootstrap3 BootstrapBasicForm $ SchuleForm
-    <$> areq textField (bfs MsgSchuleName) (fmap name mschule)
-    <*> aopt textField (bfs MsgSchuleSuffix) (fmap suffix mschule)
+  let fromName = pack . toString
+      toName = Name . unpack
+  renderBootstrap3 BootstrapBasicForm $ Schule
+    <$> pure (maybe undefined unr mschule)
+    <*> (toName <$> areq textField (bfs MsgSchuleName) (fromName . name <$> mschule))
+    <*> (toName . maybe "" id <$> aopt textField (bfs MsgSchuleSuffix) (Just . fromName . mail_suffix <$> mschule))
+    <*> pure (maybe False use_shibboleth mschule)
+    <*> areq (selectField spracheOptionen) (bfs MsgSchuleSprache) (preferred_language <$> mschule)
     <* bootstrapSubmit (BootstrapSubmit (maybe MsgSchuleAnlegen (\ _ -> MsgSchuleBearbeiten) mschule) "btn-success" [])
--- yesod/Handler/SchuleAnlegen.hs
+++ yesod/Handler/SchuleAnlegen.hs
@@ -2,15 +2,20 @@ module Handler.SchuleAnlegen where
 
 import Import
 import Handler.Schule (schuleForm)
+import Control.Schule.DB as SchuleDB
 
 getSchuleAnlegenR :: Handler Html
-getSchuleAnlegenR = do
-  (formWidget, formEnctype) <- generateFormPost $ schuleForm Nothing
-  defaultLayout $ do
-    $(widgetFile "schuleAnlegen")
+getSchuleAnlegenR = postSchuleAnlegenR
 
 postSchuleAnlegenR :: Handler Html
 postSchuleAnlegenR = do
   ((result, formWidget), formEnctype) <- runFormPost $ schuleForm Nothing
+  case result of
+    FormMissing -> return ()
+    FormFailure _ -> return ()
+    FormSuccess schule' -> do
+      _ <- lift $ SchuleDB.put Nothing schule'
+      _ <- setMessageI MsgSchuleAngelegt
+      redirect SchulenR -- ^ TODO: SchuleR verwenden (zu neu erstellter Schule gehen)
   defaultLayout $ do
     $(widgetFile "schuleAnlegen")
