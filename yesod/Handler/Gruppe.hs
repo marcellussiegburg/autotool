a16fce34f339d08710bf8badfdf0b55a2cf6d330
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2015-02-04 12:26:28 +0100

    Gruppe anlegen, bearbeiten und löschen refaktorisiert

--- yesod/Handler/Gruppe.hs
+++ yesod/Handler/Gruppe.hs
@@ -1,33 +1,65 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Handler.Gruppe where
 
 import Import
 
-data GruppeForm = GruppeForm {
-    name :: Text,
-    referent :: Text,
-    plätze :: Int
-}
+import qualified Control.Gruppe.DB as GruppeDB
+import Control.Gruppe.Typ
+import Control.Types
+
+erstellen :: Text
+erstellen = "erstellen"
+
+entfernen :: Text
+entfernen = "entfernen"
 
 getGruppeR :: GruppeId -> Handler Html
-getGruppeR gruppe = do
-  let gruppe' = Just $ GruppeForm "14IN-B" "Herr Müller" 30
-  (formWidget, formEnctype) <- generateFormPost $ gruppeForm gruppe'
-  defaultLayout $ do
-    $(widgetFile "gruppe")
+getGruppeR = postGruppeR
 
 postGruppeR :: GruppeId -> Handler Html
 postGruppeR gruppe = do
-  let gruppe' = Just $ GruppeForm "14IN-B" "Herr Müller" 30
-  ((result, formWidget), formEnctype) <- runFormPost $ gruppeForm gruppe'
+  mgruppe <- lift $ liftM listToMaybe $ GruppeDB.get_gnr $ GNr gruppe
+  ((result, formWidget), formEnctype) <- runFormPost $ identifyForm erstellen $ gruppeForm mgruppe
+  case result of
+    FormMissing -> return ()
+    FormFailure _ -> return ()
+    FormSuccess gruppe' -> do
+      _ <- lift $ GruppeDB.put (Just $ gnr gruppe') gruppe'
+      setMessageI MsgGruppeBearbeitet
+  ((entfernenResult, _), _) <- runFormPost $ identifyForm entfernen $ entfernenForm Nothing
+  (entfernenWidget, entfernenEnctype) <- case entfernenResult of
+    FormMissing ->
+      generateFormPost $ identifyForm entfernen $ entfernenForm Nothing
+    FormFailure _ ->
+      generateFormPost $ identifyForm entfernen $ entfernenForm $ Just entfernen
+    FormSuccess _ -> do
+      case mgruppe of
+        Nothing -> do
+          setMessageI MsgFehler
+          redirect SchulenR
+        Just gruppe' -> do
+          lift $ GruppeDB.delete $ gnr gruppe'
+          setMessageI MsgGruppeEntfernt
+          let VNr s = vnr gruppe'
+          redirect $ GruppenR s
   defaultLayout $ do
     $(widgetFile "gruppe")
 
-gruppeForm :: Maybe GruppeForm -> Form GruppeForm
+gruppeForm :: Maybe Gruppe -> Form Gruppe
 gruppeForm mgruppe = do
-  renderBootstrap3 BootstrapBasicForm $ GruppeForm
-    <$> areq textField (bfs MsgGruppeName) (fmap name mgruppe)
-    <*> areq textField (bfs MsgReferent) (fmap referent mgruppe)
-    <*> areq intField (bfs MsgPlätze) (fmap plätze mgruppe)
-    <* bootstrapSubmit (BootstrapSubmit (maybe MsgGruppeAnlegen (\ _ -> MsgGruppeBearbeiten) mgruppe) "btn-success" [])
-    <* maybe (pure ()) (\_ -> bootstrapSubmit (BootstrapSubmit MsgLöschen "btn-danger" [])) mgruppe
+    let fromName = pack . toString
+        toName = Name . unpack
+    renderBootstrap3 BootstrapBasicForm $ Gruppe
+      <$> pure (maybe undefined gnr mgruppe)
+      <*> pure (maybe undefined vnr mgruppe)
+      <*> (toName <$> areq textField (bfs MsgGruppeName) (fromName . name <$> mgruppe))
+      <*> areq mitgliederField (bfs MsgPlätze) (maxStudents <$> mgruppe)
+      <*> (toName <$> areq textField (bfs MsgReferent) (fromName . referent <$> mgruppe))
+      <* bootstrapSubmit (BootstrapSubmit (maybe MsgGruppeAnlegen (\ _ -> MsgGruppeBearbeiten) mgruppe) "btn-success" [])
+  where
+    mitgliederField = checkBool (> 0) MsgGruppeZuKlein intField
+
+entfernenForm :: Maybe Text -> Form Text
+entfernenForm mentfernen = do
+  renderBootstrap3 BootstrapBasicForm $
+    areq hiddenField (bfs $ maybe MsgLöschen (\_ -> MsgGruppeEntfernen) mentfernen) mentfernen
+    <* bootstrapSubmit (BootstrapSubmit MsgLöschen "btn-danger" [])
--- yesod/Handler/GruppeAnlegen.hs
+++ yesod/Handler/GruppeAnlegen.hs
@@ -3,14 +3,26 @@ module Handler.GruppeAnlegen where
 import Import
 import Handler.Gruppe (gruppeForm)
 
+import qualified Control.Vorlesung.DB as VorlesungDB
+import qualified Control.Vorlesung.Typ as Vorlesung
+import qualified Control.Gruppe.DB as GruppeDB
+import Control.Gruppe.Typ
+import Control.Types
+
 getGruppeAnlegenR :: VorlesungId -> Handler Html
-getGruppeAnlegenR vorlesung = do
-  (formWidget, formEnctype) <- generateFormPost $ gruppeForm Nothing
-  defaultLayout $ do
-    $(widgetFile "gruppeAnlegen")
+getGruppeAnlegenR = postGruppeAnlegenR
 
 postGruppeAnlegenR :: VorlesungId -> Handler Html
 postGruppeAnlegenR vorlesung = do
   ((result, formWidget), formEnctype) <- runFormPost $ gruppeForm Nothing
+  case result of
+    FormMissing -> return ()
+    FormFailure _ -> return ()
+    FormSuccess gruppe' -> do
+      Just vorlesung' <- lift $ liftM listToMaybe $ VorlesungDB.get_this $ VNr vorlesung
+      _ <- lift $ GruppeDB.put Nothing gruppe' { vnr = Vorlesung.vnr vorlesung' }
+      _ <- setMessageI MsgGruppeAngelegt
+      let VNr v = Vorlesung.vnr vorlesung'
+      redirect $ GruppenR v -- ^ TODO: GruppeR verwenden (zu neu erstellter Übungsgruppe gehen)
   defaultLayout $ do
     $(widgetFile "gruppeAnlegen")
