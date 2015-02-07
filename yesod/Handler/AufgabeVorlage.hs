9aa2bbc5e93dace50a9a03ed6bc48cd6e808b186
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2015-02-07 10:49:53 +0100

    Refaktorisierung des Abrufen der Aufgabenkonfiguration

--- yesod/Handler/AufgabeVorlage.hs
+++ yesod/Handler/AufgabeVorlage.hs
@@ -1,9 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Handler.AufgabeVorlage where
 
+import qualified Control.Exception as Exception
 import Import
+import qualified Control.Aufgabe.DB as AufgabeDB
+import Control.Aufgabe.Typ (Aufgabe (name, config))
+import Control.Types
+import Service.Interface (get_task_description_localized)
+import Types.Config (Config (CString))
+import Types.Documented (contents)
+import Types.TaskDescription (task_sample_config)
 
 getAufgabeVorlageR :: ServerUrl -> AufgabeTyp -> VorlageName -> Handler Html
 getAufgabeVorlageR server aufgabeTyp vorlageName = do
-  let konfiguration = "[ ( Pre\n  , [ e , j , b , i , f , m , l\n    , k , d , g , c , a , h ] )\n, ( In\n  , [ b , j , i , e , k , l , d\n    , m , g , f , a , c , h ] ) ]"
-  redirect $ AufgabeKonfigurationR server aufgabeTyp konfiguration
+  aufgaben <- lift $ AufgabeDB.get_typed (fromCGI $ unpack aufgabeTyp)
+      `Exception.catch` \ (Exception.SomeException _) -> return []
+  let maufgabe = listToMaybe $ filter (\a -> vorlageName == (pack $ toString $ name a)) aufgaben
+  sprache <- getBevorzugteSprache
+  CString konfiguration <- case maufgabe of
+    Nothing -> lift $ liftM (contents . task_sample_config)
+                    $ get_task_description_localized (unpack server) (unpack aufgabeTyp) sprache
+    Just aufgabe -> return $ CString $ toString $ config aufgabe
+  redirect $ AufgabeKonfigurationR server aufgabeTyp $ pack konfiguration
