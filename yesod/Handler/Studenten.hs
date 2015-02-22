b28e322cab3d3e38561da88f41ba9aa499cf6fbd
Marcellus Siegburg <msiegbur@imn.htwk-leipzig.de>
2015-02-22 11:38:53 +0100

    Refaktorisierung des Anzeigen der Resultate

--- yesod/Handler/Studenten.hs
+++ yesod/Handler/Studenten.hs
@@ -3,10 +3,14 @@ module Handler.Studenten where
 import Import
 import Data.List (nub)
 
+import qualified Control.Aufgabe.DB as AufgabeDB
+import qualified Control.Aufgabe.Typ as Aufgabe
 import qualified Control.Gruppe.DB as GruppeDB
 import qualified Control.Gruppe.Typ as Gruppe
 import qualified Control.Student.DB as StudentDB
 import qualified Control.Student.Type as Student
+import qualified Control.Stud_Aufg.DB as EinsendungDB
+import qualified Control.Stud_Aufg.Typ as Einsendung
 import qualified Control.Vorlesung.DB as VorlesungDB
 import qualified Control.Types as T
 
@@ -14,11 +18,13 @@ data BewertungListe = BewertungEintrag {
   student :: Student.Student,
   gruppe :: Int,
   punkte :: Int,
-  ergebnisse :: [Ergebnis]
+  ergebnisse :: [Maybe T.Wert]
 }
 
-type Ergebnis = Maybe (Int, Int)
-data Aufgaben = KeineAufgaben | PflichtAufgaben | AlleAufgaben
+data Aufgaben =
+  KeineAufgaben |
+  PflichtAufgaben |
+  AlleAufgaben deriving Eq
 
 titel :: Aufgaben -> AutotoolMessage
 titel aufgaben = case aufgaben of
@@ -44,20 +50,25 @@ danach = davor . davor
 getStudentenR :: VorlesungId -> Handler Html
 getStudentenR = studentenListe KeineAufgaben
 
-studentenListe :: Aufgaben -> Int -> Handler Html
+studentenListe :: Aufgaben -> VorlesungId -> Handler Html
 studentenListe auswahl vorlesung = do
   einschreibungen <- liftIO $ VorlesungDB.snr_gnr_teilnehmer $ T.VNr vorlesung
   gruppen <- liftIO $ liftM concat $ mapM GruppeDB.get_gnr $ nub $ fmap snd einschreibungen
+  alleAufgaben <- liftIO $ AufgabeDB.get $ Just $ T.VNr vorlesung
+  let aufgaben = do
+        aufgabe <- alleAufgaben
+        guard $ case auswahl of
+          KeineAufgaben -> False
+          PflichtAufgaben -> Aufgabe.status aufgabe == T.Mandatory
+          AlleAufgaben -> True
+        return aufgabe
   bewertungen <- liftIO $ liftM concat $ sequence $ do
     (snr, T.GNr gruppeId) <- einschreibungen
     return $ liftM maybeToList $ runMaybeT $ do
       mstudent <- lift $ StudentDB.get_snr snr
       student' <- MaybeT . return . listToMaybe $ mstudent
-      return $ BewertungEintrag student' gruppeId 0 []
+      einsendungen <- lift $ mapM (EinsendungDB.get_snr_anr snr . Aufgabe.anr) aufgaben
+      return $ BewertungEintrag student' gruppeId 0 $ fmap (maybe Nothing Einsendung.result . listToMaybe) einsendungen
   let fromGNr gnr = let T.GNr g = gnr in g
-      aufgaben = case auswahl of
-        KeineAufgaben -> []
-        PflichtAufgaben -> ["peano-fold-times"] :: [Text]
-        AlleAufgaben -> ["guess-the-number", "multiply-peano-numbers", "peano-fold-times"] :: [Text]
   defaultLayout $ do
     $(widgetFile "studenten")
