96ad334ce9f4e997e53e36b10b5d6f6940c263a5
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-04-26 18:53:26 +0200

    fractran

--- collection/src/Inter/Collector.hs
+++ collection/src/Inter/Collector.hs
@@ -169,6 +169,7 @@ import qualified KnapsackFraction.Central
 import qualified RAM.Make
 import qualified Goto.Make
 import qualified While.Make
+import qualified Fractran.Machine
 
 import qualified Program.General.Central
 import qualified Program.Array.Instance
@@ -450,6 +451,9 @@ tmakers =
                   [ item While.Make.make 
                   , item RAM.Make.make
                   ]
+                , heading "Fractran-Programme"
+                    [ item Fractran.Machine.make
+                    ]
 
                 -- , item RM.Make.make
                   
