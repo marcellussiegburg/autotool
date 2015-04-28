7ebf8422a84784a8efdfa86b35ff82c32e26b782
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-04-28 13:51:30 +0200

    add remark

--- collection/src/Compiler/Normalform.hs
+++ collection/src/Compiler/Normalform.hs
@@ -6,5 +6,9 @@ import qualified Goto as G
 import qualified Compiler.While_Goto as WG
 import qualified Compiler.Goto_While as GW
 
+-- | konstruktiver Beweis für den Satz (von Kleene):
+-- zu jedem While-Programm gibt es ein äquivalentes
+-- mit genau einem While.
+
 normal :: W.Program -> W.Program
 normal = GW.compile . WG.compile
