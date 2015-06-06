404729b379e85a861cdd704c963d36eea64b9546
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-06-06 19:44:11 +0200

    version

--- build-doc.sh
+++ build-doc.sh
@@ -2,11 +2,13 @@
 
 LIBS="derive transport todoc output reader data tex reporter util algorithm dot relation fa genetic rewriting graph exp logic fta foa"
 
+GHC=7.8.4
+
 echo IF THIS FAILS STRANGELY: check the hard-coded ghc version in the script
 
 standalone-haddock \
     --hyperlink-source \
     -o /var/www/autotool/docs \
-    --package-db $HOME/.ghc/x86_64-linux-7.8.4/package.conf.d \
+    --package-db $HOME/.ghc/x86_64-linux-$GHC/package.conf.d \
     $(for lib in $LIBS ; do echo "../autolib/$lib" ; done ) \
     interface collection
